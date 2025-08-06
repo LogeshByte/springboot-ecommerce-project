e21ef60f911a6269d0c1b2352a021b83bcf0ff30
Logeshwar <calogeshwar@gmail.com>
2025-08-06 18:46:47 +0530

    Implemented controller and service layers with basic GetMapping and PostMapping endpoints

---
+++ src/main/java/com/logesh/ecom_project/service/CategoryServiceImpl.java
@@ -0,0 +1,25 @@
+package com.logesh.ecom_project.service;
+
+import com.logesh.ecom_project.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+    private List<Category> categories = new ArrayList<>();
+    private Long nextId =1L;
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
+        categories.add(category);
+    }
+}
